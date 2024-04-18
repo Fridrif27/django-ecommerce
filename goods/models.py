0454675d5b94e3d5068a08615651cef177be5046
Polina <polina_kupreychuk@knu.ua>
2024-04-18 21:37:57 +0300

    add icons

--- goods/models.py
+++ goods/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Name')
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    icon = models.CharField(max_length=50, blank=True, null=True, verbose_name='Icon')
 
     class Meta:
         db_table = 'category'

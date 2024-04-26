28731e8551c91b55dedafb086b2173a4831cc79c
Maksym Havryshko <maksymhavryshko@MacBook-Pro-Maksym.local>
2024-04-26 18:13:35 +0300

    def catalog updated

--- goods/views.py
+++ goods/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render
 from goods.models import Products
 
 
-def catalog(request):
-    
-    goods = Products.objects.all()
-
+def catalog(request, category_slug):
+    if category_slug == "all":
+        goods = Products.objects.all()
+    else:
+        goods = get_object_or_404(Products.objects.filter(category__slug=category_slug))
     context = {
         "title": "Home - Catalog",
         "goods": goods,
@@ -15,12 +16,11 @@ def catalog(request):
     return render(request, "goods/catalog.html", context)
 
 
-def product(request):
+def product(request, product_slug):
 
     product = Products.objects.get(slug=product_slug)
 
-    context = {
-        'product': product
-    }
+    context = {"product": product}
+    
 
     return render(request, "goods/product.html")
